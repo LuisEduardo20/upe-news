4b0d36c7d1c2efe8027c1e31ade2647276241fb9
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-08-04 20:11:13 -0300

    feat: databases conections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Olá mundo!");
+import cron from "node-cron";
+import { poolMaster, poolSlave } from "./services/databases";
+
+cron.schedule("* * * * *", () => {
+  console.log("Conectando aos bancos");
+
+  poolMaster.connect();
+  poolSlave.connect();
+});
---
+++ src/services/databases.ts
@@ -0,0 +1,45 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const poolMaster = new Pool({
+  host: process.env.DB_MASTER_HOST,
+  database: process.env.DB_MASTER_DATABASE,
+  user: process.env.DB_MASTER_USER,
+  password: process.env.DB_MASTER_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export const poolSlave = new Pool({
+  host: process.env.DB_SLAVE_HOST,
+  database: process.env.DB_SLAVE_DATABASE,
+  user: process.env.DB_SLAVE_USER,
+  password: process.env.DB_SLAVE_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+poolMaster.on("error", (err, client) => {
+  console.error("Error on master database", err);
+  process.exit(-1);
+});
+
+poolMaster.on("connect", (err) => {
+  console.error("Conectou no master");
+});
+
+poolSlave.on("error", (err, client) => {
+  console.error("Error on slave database:", err);
+  process.exit(-1);
+});
+
+poolSlave.on("connect", (err) => {
+  console.error("Conectou no slave");
+});
