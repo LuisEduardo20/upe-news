171aad2b12e3c2f690e60ebc9cb79742d32ad692
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-08-07 17:32:33 -0300

    feat: getting data from database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import cron from "node-cron";
 import { poolMaster, poolSlave } from "./services/databases";
 
+// cron.schedule("* * * * *", () => {
 cron.schedule("* * * * *", () => {
   console.log("Conectando aos bancos");
 
   poolMaster.connect();
+  poolMaster.query(`SELECT * FROM news`, (err, res) => {
+    console.log("res:", res.rows);
+  });
+
   poolSlave.connect();
+  poolSlave.query(`SELECT * FROM news`, (err, res) => {
+    console.log("res:", res.rows);
+  });
 });
